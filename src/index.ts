bd27ef486bc33788a485ee7d0d4760139e518b35
denfasyah <denfasyah@gmail.com>
2026-05-28 09:53:11 +0700

    Update default PORT value from 3000 to 3001

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import path from "path";
 import express from "express";
 import cors from "cors";
 
-const PORT = process.env.PORT || animeConfig.PORT || 3000;
+const PORT = process.env.PORT || animeConfig.PORT || 3001;
 const app = express();
 
 // MIDDLEWARES
